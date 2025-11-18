3bf5d6f705abe01fc051a8745261d32d848197ee
rta228 <rta228gta@gmail.com>
2025-11-18 15:36:18 +0100

    I dont know

---
+++ Zadania-06/Projekt6/Projekt6/Projekt6.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int a = 20;
+int b = 10;
+
+int sumOf(int pA, int pB) {
+	return pA + pB;
+}
+
+int main(){
+    //int a = 2;
+	int b = 1;
+
+    cout << "Oto suma: " << sumOf(a,b) << "!\n";
+}
